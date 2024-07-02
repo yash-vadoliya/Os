b5f0e375bb3ba2cf14788d9ecc79ae9ec7802a23
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2024-07-02 13:25:12 +0530

    Add files via upload

---
+++ main.bash
@@ -0,0 +1,67 @@
+# 
+# Welcome to GDB Online.
+# GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+# C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+# Code, Compile, Run and Debug online from anywhere in world.
+# 
+# 
+# echo "Hello World";
+
+# ====================
+
+# pwd
+
+# mkdir yash
+
+# ls
+
+# rmdir yash
+
+# ls
+
+# cal
+
+# date
+
+# cd yash
+
+# pwd
+
+# cd ..
+
+# pwd
+
+# touch one.txt
+
+# cat one.txt
+
+# head -n 2 one.txt
+
+# head -c 23 one.txt
+
+# tail -n 2 one.txt
+
+# tail -c 23 one.txt
+
+# tail --version
+
+# who
+
+wc one.txt
+# ============
+
+# # pwd
+# mkdir Yash
+# # ls
+# # cd abc
+# # touch one.txt
+# # touch two.txt
+# head -n 4 one.txt
+# tail -n 3 two.txt
+
+# cd ..
+# mkdir abc2
+# cd abc2
+# touch first.txt
+
+ls -l
